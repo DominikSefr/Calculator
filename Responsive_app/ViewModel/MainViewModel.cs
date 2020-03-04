769cf1f2a2c603faa6868db5342ba39e51f2cf82
unknown <Administrator@PC2019-08.ad.pslib.cz>
2020-03-04 09:40:13 +0100

    fast

--- Responsive_app/ViewModel/MainViewModel.cs
+++ Responsive_app/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +15,6 @@ namespace Responsive_app.ViewModel
         private string _storedValue;
        // private CalculatorOperation _operation = CalculatorOperation.None;
         private string _message;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public ParametrizedRelayCommand Number { get; set; }
         public ParametrizedRelayCommand Operation { get; set; }
         public ParametrizedRelayCommand Function { get; set; }
@@ -24,10 +22,49 @@ namespace Responsive_app.ViewModel
         public RelayCommand Point { get; set; }
         public RelayCommand Sign { get; set; }
         public RelayCommand Result { get; set; }
+        public string TextResult
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                NotifyPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
-           /* Number = new ParametrizedRelayCommand(
-                (param) => );*/
+            Number = new ParametrizedRelayCommand(
+                 (param) =>
+                    TextResult += param
+                 );
+            Reset = new RelayCommand(
+                () =>
+                {
+                    TextResult = "";
+                }
+                );
+            Point = new RelayCommand(
+                () =>
+                {
+                    if (!TextResult.Contains(".") && TextResult.Length > 0)
+                    {
+                        TextResult += "."; 
+                    }
+                }
+                );
+            Sign = new RelayCommand(
+                () =>
+                {
+                    TextResult = Convert.ToString(Convert.ToDouble(TextResult) * -1);
+                }
+                );
+
         }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
